3906736119acdc88a0c4184686813a389e2ddc34
Tom Donohue <monodot@gmail.com>
2021-04-11 18:16:09 +0100

    Initial commit

---
+++ src/main/java/com/tutorialworks/hellojavaspringboot/HelloJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialworks.hellojavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorialworks/hellojavaspringboot/HelloJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialworks.hellojavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
